c799d968cd88f96b058a61d10a028f872c91a19b
Rana Muzamal <muzamilrana149@gmail.com>
2022-11-05 02:59:38 -0700

    updated file

--- vowels.js
+++ vowels.js
@@ -1,5 +1,4 @@
 var a="dfjekjdvcgdjovksjdopbkcdpowsbjkqjjbjzsdwcxkkjcx hj ko jkqws kkc jks jxkqk";
 function vowel(x){
-    if(x=='a' || x=='e' || x=='i'|| x=='o'|| x=='u'|| x=='a' || x=='A' || x=='I'|| x=='O'|| x=='U'){ 
-    }
+    This is updated file
 }
