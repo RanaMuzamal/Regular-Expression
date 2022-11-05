ac471b7c5c3f1b1dbf2fb3a250ffbd83e41a7d07
Rana Muzamal <muzamilrana149@gmail.com>
2022-11-05 02:49:17 -0700

    create program to find vowels from string

---
+++ vowels.js
@@ -0,0 +1,5 @@
+var a="dfjekjdvcgdjovksjdopbkcdpowsbjkqjjbjzsdwcxkkjcx hj ko jkqws kkc jks jxkqk";
+function vowel(x){
+    if(x=='a' || x=='e' || x=='i'|| x=='o'|| x=='u'|| x=='a' || x=='A' || x=='I'|| x=='O'|| x=='U'){ 
+    }
+}
